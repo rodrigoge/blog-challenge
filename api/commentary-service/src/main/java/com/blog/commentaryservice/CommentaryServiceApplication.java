31c63cb46229c8ba1c5433949a489dca31cbe1cb
Rodrigo Gouveia <rgestevao@gmail.com>
2023-10-28 23:13:23 -0300

    feat: starting the service project

---
+++ api/commentary-service/src/main/java/com/blog/commentaryservice/CommentaryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.blog.commentaryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentaryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentaryServiceApplication.class, args);
+	}
+
+}
---
+++ api/commentary-service/src/test/java/com/blog/commentaryservice/CommentaryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.commentaryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentaryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
